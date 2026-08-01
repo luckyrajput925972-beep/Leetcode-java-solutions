9c734ea5cce02a2e17ca75c42da524f83a09cade
Lalit singh <luckyrajput925972@gmail.com>
2026-08-01 22:51:16 +0530

    Time: 1 ms (100.00%) | Memory: 44.1 MB (83.23%) - LeetSync

---
+++ 223-rectangle-area/rectangle-area.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int overlap=0;
+        int area1=(ax2-ax1)*(ay2-ay1);
+        int area2=(bx2-bx1)*(by2-by1);
+        int overlapWidth = Math.min(ax2, bx2) - Math.max(ax1, bx1);
+        int overlapheight= Math.min(ay2, by2) - Math.max(ay1, by1);
+        if(overlapWidth>0 && overlapheight>0){
+            overlap=overlapWidth*overlapheight;
+
+        }
+        return area1+area2-overlap;
+    }
+}
