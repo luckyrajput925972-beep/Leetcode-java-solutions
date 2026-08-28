e2b15af2388b728e73e6bf387bb69402c454ae27
Lalit singh <luckyrajput925972@gmail.com>
2026-08-28 22:32:22 +0530

    Time: 1 ms (94.74%) | Memory: 46.6 MB (13.57%) - LeetSync

--- 4354-unique-middle-element/unique-middle-element.java
+++ 4354-unique-middle-element/unique-middle-element.java
@@ -1,20 +1,12 @@
 class Solution {
     public boolean isMiddleElementUnique(int[] nums) {
         int count=0;
-        
-
-
     for(int i=0;i<nums.length;i++){
         if(nums[nums.length/2]==nums[i]){
             count++;
         }
     }
     if(count==1)return true;
-    
-
-        
-
-
         return false;
     }
 }
