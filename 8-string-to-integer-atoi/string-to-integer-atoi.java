29d9db05958b9487fff3840e9ad396567d489bf7
Lalit singh <luckyrajput925972@gmail.com>
2026-09-19 23:23:44 +0530

    Time: 1 ms (100.00%) | Memory: 43.7 MB (57.62%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int myAtoi(String s) {
+
+        int i = 0;
+        int sign = 1;
+        int num = 0;
+
+        
+        while (i < s.length() && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        
+        if (i < s.length() && s.charAt(i) == '-') {
+            sign = -1;
+            i++;
+        } 
+        else if (i < s.length() && s.charAt(i) == '+') {
+            i++;
+        }
+
+        
+        while (i < s.length() && Character.isDigit(s.charAt(i))) {
+
+            int digit = s.charAt(i) - '0';
+
+            
+            if (num > (Integer.MAX_VALUE - digit) / 10) {
+                if (sign == 1)
+                    return Integer.MAX_VALUE;
+                else
+                    return Integer.MIN_VALUE;
+            }
+
+            num = num * 10 + digit;
+            i++;
+        }
+
+        return num * sign;
+    }
+}
